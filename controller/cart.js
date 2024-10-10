8d6483cf3ea90f8d7f5c327a40dd6e84a1c56391
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-10-11 00:51:32 +0530

    feat: Implemented logic for adding to cart.
    For each new dish that's being added to the cart, check if it exists,
    if so then just increment the count else create a new entry
    
    For updating, just check if count is a valid non negative integer then
    update.

--- controller/cart.js
+++ controller/cart.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const Cart = require("../model/cart");
 const Dish = require("../model/dish");
+const { set } = require("mongoose");
 exports.getCartDetails = async (req, res) => {
     try {
         Cart.hasMany(Dish, {
@@ -8,33 +9,93 @@ exports.getCartDetails = async (req, res) => {
             sourceKey: "dishId",
         });
         const cart = await Cart.findAll({
-            attributes: ["*", "Cart.*"],
+            attributes: ["*"],
             include: [
                 {
                     model: Dish,
                     required: true,
+                    raw: true,
                 },
             ],
-            where: { userId: req.params.userId },
+            where: { userId: req.params.id },
+            raw: true,
+            nest: true,
         });
+        // console.log(cart);
+        // var dishArr = [];
+        // for (let i = 0; i < cart.length; i++) {
+        //     dishArr.push(cart[i].Dishes[0]);
+        // }
         return res.json({ status: true, data: cart });
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message });
     }
 };
 exports.addToCart = async (req, res) => {
-    const newCartItem = await Cart.create(req.body);
-    return res.status(201).json(newCartItem);
+    /**
+     * Check if the dish already exists:
+     *      if yes then update the counter
+     *      else add it to the list
+     */
+
+    // const newCartItem = await Cart.create(req.body);
+    try {
+        const checkCart = await Cart.findOne({
+            where: {
+                userId: req.body.userId,
+                dishId: req.body.dishId,
+            },
+        });
+        let cart;
+
+        if (!checkCart) {
+            cart = await Cart.create(req.body);
+        } else {
+            let newCount = checkCart.count + (req.body.count || 1);
+            if (newCount < 0) {
+                newCount = 0;
+            }
+            // console.log(newCount);
+            await Cart.update(
+                { count: newCount },
+                {
+                    where: {
+                        userId: req.body.userId,
+                        dishId: req.body.dishId,
+                    },
+                }
+            );
+            cart = await Cart.findOne({
+                where: {
+                    userId: req.body.userId,
+                    dishId: req.body.dishId,
+                },
+            });
+        }
+
+        return res.json({ status: true, data: cart });
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message });
+    }
 };
 exports.updateCart = async (req, res) => {
-    const [updatedRowsCount, updatedMenus] = await Cart.update(req.body, {
-        where: { id: req.params.id },
-        returning: true,
-    });
+    try {
+        if (req.body.count && req.body.count < 0) {
+            req.body.count = 0;
+        }
+        const [updatedRowsCount, updatedCarts] = await Cart.update(req.body, {
+            where: {
+                userId: req.body.userId,
+                dishId: req.body.dishId,
+            },
+            returning: true,
+        });
 
-    if (updatedRowsCount === 0) {
-        return res.status(404).json({ message: "Menu not found" });
+        if (updatedRowsCount === 0) {
+            return res.status(404).json({ message: "Cart not found" });
+        }
+        return res.json({ status: true, data: updatedCarts });
+    } catch (error) {
+        return res.status(500).json({ status: false, message: error.message });
     }
-
-    return res.json({ msg: "Successfully Updated!" });
 };
