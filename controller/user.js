8ebdd6a585ddb71bf39923c64d1295d86fcdc7e9
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-11-11 01:59:03 +0530

    fix: minor fixes

--- controller/user.js
+++ controller/user.js
@@ -3,6 +3,11 @@ const User = require("../model/user");
 exports.createUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
+        if (newUser) {
+            return res.status(201).json({ status: true, user: newUser });
+        } else {
+            return res.status(409).json({ status: false, message: "bad request" });
+        }
     } catch (error) {
         return res.status(500).json({ status: false, message: error.message });
     }
@@ -10,8 +15,8 @@ exports.createUser = async (req, res) => {
 
 exports.getUser = async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { id: req.id }, raw: true });
-        if (newUser) {
+        const userData = await User.findOne({ where: { id: req.params.id }, raw: true });
+        if (userData) {
             return res.status(200).json({ msg: "Found", user: userData });
         } else {
             return res.status(404).json({ msg: "Not Found", user: null });
--- routes/user.js
+++ routes/user.js
@@ -4,3 +4,5 @@ const userController = require("../controller/user");
 const userRouter = express.Router();
 userRouter.get("/api/user/:id", userController.getUser);
 userRouter.post("/api/user/", userController.createUser);
+
+module.exports = userRouter;
