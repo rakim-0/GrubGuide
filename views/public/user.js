666d52182fdf0767b88ef4cce1eaa0c2e3150279
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-11-11 02:29:04 +0530

    fix: unset userid in localstorage when user logs in

--- views/public/user.js
+++ views/public/user.js
@@ -8,20 +8,39 @@ function checkIfUserIdExists() {
 function setUserId(userId) {
     localStorage.setItem("userId", userId);
 }
+function removeUserId() {
+    localStorage.removeItem("userId");
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-function generateUserId() {
-    const newUserId = 9000 + getRandomInt(1000); // 9000 to 9999 is reserved for random userId
+async function checkUserId(id) {
+    let response = await fetch(`http://localhost:8080/api/user/${id}`);
+    return response.status == 200;
+}
+async function generateUserId() {
+    let newUserId = getRandomInt(10000); // 9000 to 9999 is reserved for random userId
+    while (await checkUserId(newUserId)) {
+        newUserId++;
+    }
     return newUserId;
 }
 
+function checkLogin() {
+    const loggedin = document.getElementById("logged-in");
+    if (loggedin) {
+        return true;
+    }
+    return false;
+}
 
-window.onload = function () {
-    const userid = document.getElementById("userid");
-    if (userid) {
-        setUserId(userid.textContent);
+window.onload = async function () {
+    if (checkLogin()) {
+        removeUserId();
     } else {
-        setUserId(generateUserId());
+        if (!checkIfUserIdExists()) {
+            setUserId(await generateUserId());
+        }
     }
 };
